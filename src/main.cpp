2b68c1f7049dd6f42ff91148c0da1d2c32fe6ca6
GOROman <yoshihito.kondoh@gmail.com>
2025-02-08 14:28:42 +0900

    feat: キーボード入力テスト用のコードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,71 @@
-#include <Arduino.h>
+/**
+ * @file main.cpp
+ * @brief M5Cardputer input text example
+ * @version 0.1
+ * @date 2024-02-08
+ *
+ * @Hardwares: M5Cardputer
+ * @Platform Version: Arduino M5Stack Board Manager v2.0.7
+ * @Dependent Library:
+ * M5GFX: https://github.com/m5stack/M5GFX
+ * M5Unified: https://github.com/m5stack/M5Unified
+ */
 
-// put function declarations here:
-int myFunction(int, int);
+#include "M5Cardputer.h"
+#include "M5GFX.h"
+
+M5Canvas canvas(&M5Cardputer.Display);
+String data = "> ";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    auto cfg = M5.config();
+    M5Cardputer.begin(cfg, true);
+    M5Cardputer.Display.setRotation(1);
+    M5Cardputer.Display.setTextSize(0.5);
+    M5Cardputer.Display.drawRect(0, 0, M5Cardputer.Display.width(),
+                                 M5Cardputer.Display.height() - 28, GREEN);
+    M5Cardputer.Display.setTextFont(&fonts::FreeSerifBoldItalic18pt7b);
+
+    M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 4,
+                                 M5Cardputer.Display.width(), 4, GREEN);
+
+    canvas.setTextFont(&fonts::FreeSerifBoldItalic18pt7b);
+    canvas.setTextSize(0.5);
+    canvas.createSprite(M5Cardputer.Display.width() - 8,
+                        M5Cardputer.Display.height() - 36);
+    canvas.setTextScroll(true);
+    canvas.println("Press Key and Enter to Input Text");
+    canvas.pushSprite(4, 4);
+    M5Cardputer.Display.drawString(data, 4, M5Cardputer.Display.height() - 24);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    M5Cardputer.update();
+    if (M5Cardputer.Keyboard.isChange()) {
+        if (M5Cardputer.Keyboard.isPressed()) {
+            Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+            for (auto i : status.word) {
+                data += i;
+            }
+
+            if (status.del) {
+                data.remove(data.length() - 1);
+            }
+
+            if (status.enter) {
+                data.remove(0, 2);
+                canvas.println(data);
+                canvas.pushSprite(4, 4);
+                data = "> ";
+            }
+
+            M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 28,
+                                         M5Cardputer.Display.width(), 25,
+                                         BLACK);
+
+            M5Cardputer.Display.drawString(data, 4,
+                                           M5Cardputer.Display.height() - 24);
+        }
+    }
 }
